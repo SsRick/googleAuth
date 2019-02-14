3c4876290212d9db2572764fcd2e5e4b4e14d0aa
SsRick <shankhashubhra96@gmail.com>
2019-02-14 15:20:45 +0530

    fixed request

--- auth2token.py
+++ auth2token.py
@@ -1,13 +1,25 @@
-import requests
+# importing the requests library 
+import requests 
 
-url = "https://oauth2.googleapis.com/token"
+# defining the api-endpoint 
+API_ENDPOINT = "https://oauth2.googleapis.com/token"
 
-payload = "code=4%2F8QC1hgVA6Bo7NvHjrjCLwx4pfjsBGMYr--cY3fQ9fUTd5iHYm0_zLYROHR-j66ZW_HHZcyxGkLmJzvOa8siCLPo&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F&client_id=887597828597-kjg09tgnbhd6sc8pr8r32v71168v8dd7.apps.googleusercontent.com&client_secret=-WwzKK48Y5Mq6wa5RhvUAV-1&grant_type=authorization_code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&access_type=offline&undefined="
-headers = {
-    'cache-control': "no-cache",
-    'Postman-Token': "daa12b11-efa7-4e9a-ba44-327050a18859"
-    }
+# your API key here 
 
-response = requests.request("POST", url, data=payload)
+# your source code here 
 
-print(response.text)
+# data to be sent to api 
+data = {'refresh_token':'1/pVDT1QqFEtPW9GKuWd57wMDilASnFQ6DOyUgRIPyxu0', 
+      'redirect_uri':'http://localhost:8080/', 
+      'client_id':'887597828597-kjg09tgnbhd6sc8pr8r32v71168v8dd7.apps.googleusercontent.com', 
+      'client_secret':'-WwzKK48Y5Mq6wa5RhvUAV-1',
+      'grant_type' : 'refresh_token',
+      'scope' : 'https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/userinfo.profile',
+      'access_type' : 'offline'} 
+
+# sending post request and saving response as response object 
+r = requests.post(url = API_ENDPOINT, data = data) 
+
+# extracting response text 
+response = r.text 
+print("The pastebin URL is:%s"%response) 
