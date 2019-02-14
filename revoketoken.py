759417a103f1f1d4d30a05c474c1a8afe2b777e7
SsRick <shankhashubhra96@gmail.com>
2019-02-14 16:34:27 +0530

    added revoke token code

---
+++ revoketoken.py
@@ -0,0 +1,5 @@
+import requests
+
+requests.post('https://oauth2.googleapis.com/revoke',
+    params={'token': 'ya29.GluwBrgcnzE24i5Wq7p8JbincM3UdME2cDkVzff8bOuj42emIMMdookhr2nqsf-ZikW9weawAxT9qAizwph3ZvHl7HWGd5SYUF0ZZmE_UZ3jbx3BuKXFMIaM_6Rr'},
+    headers = {'content-type': 'application/x-www-form-urlencoded'})
