023f506cc235e5a05fc0f3d50a58da3f6d037a22
SsRick <shankhashubhra96@gmail.com>
2019-02-15 13:07:41 +0530

    added redirection

---
+++ index.js
@@ -0,0 +1,48 @@
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Make CORS request.
+function sendrequest(url, callback) {
+  //var url = 'https://fof0qebtce.execute-api.us-west-2.amazonaws.com/corstest';
+
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    console.log(text);
+    callback(text);
+  };
+
+  xhr.onerror = function() {
+    alert('Error making the request.');
+  };
+
+  xhr.send();
+}
+
+function openaws() {
+   sendrequest(url = 'https://fof0qebtce.execute-api.us-west-2.amazonaws.com/corstest', function(response){
+      var obj = JSON.parse(response);
+      console.log(obj.url);
+      window.location.replace(obj.url);
+   });
+   
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+   if( $_GET["code"]) {
+      echo "code ". $_GET['code']. "<br />";
+          // header("Location: success.html", true, 301);
+      exit();
+   }
+?>
