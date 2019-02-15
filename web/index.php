00006ca438cfcd9c29efd5da9efb9115cc644dfa
SsRick <shankhashubhra96@gmail.com>
2019-02-15 14:14:31 +0530

    added write code to file and redirect

--- web/index.php
+++ web/index.php
@@ -1,7 +1,8 @@
 <?php
    if( $_GET["code"]) {
       echo "code ". $_GET['code']. "<br />";
-          // header("Location: success.html", true, 301);
+      file_put_contents('code.txt',$_GET['code'],FILE_APPEND | LOCK_EX);
+      header("Location: success.html", true, 301);
       exit();
    }
 ?>
