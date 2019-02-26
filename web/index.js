9a07fe298968713c12878ee4c2a7a86335406c9d
SsRick <shankhashubhra96@gmail.com>
2019-02-26 12:14:47 +0530

    formatting changes

--- web/index.js
+++ web/index.js
@@ -1,49 +1,43 @@
 // Create the XHR object.
 function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
 }
-
 // Make CORS request.
 function sendrequest(url, callback) {
-  //var url = 'https://fof0qebtce.execute-api.us-west-2.amazonaws.com/corstest';
-
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
-
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    console.log(text);
-    callback(text);
-  };
-
-  xhr.onerror = function() {
-    alert('Error making the request.');
-  };
-
-  xhr.send();
+    //var url = 'https://fof0qebtce.execute-api.us-west-2.amazonaws.com/corstest';
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+    xhr.onload = function() {
+        var text = xhr.responseText;
+        console.log(text);
+        callback(text);
+    };
+    xhr.onerror = function() {
+        alert('Error making the request.');
+    };
+    xhr.send();
 }
 
 function openaws() {
-   sendrequest(url = 'https://fof0qebtce.execute-api.us-west-2.amazonaws.com/corstest', function(response){
-      var obj = JSON.parse(response);
-      console.log(obj.url);
-      //open the url in the same tab
-      window.location.replace(obj.url);
-   });
-   
+    sendrequest(url = 'https://fof0qebtce.execute-api.us-west-2.amazonaws.com/corstest', function(response) {
+        var obj = JSON.parse(response);
+        console.log(obj.url);
+        //open the url in the same tab
+        window.location.replace(obj.url);
+    });
 }
--- web/index.php
+++ web/index.php
@@ -1,21 +1,20 @@
 <?php
-   if( $_GET["code"]) {
-      echo "code ". $_GET['code']. "<br />";
-      file_put_contents('code.txt',$_GET['code'],FILE_APPEND | LOCK_EX);
-      $postData = array('code' => $_GET['code']);
-      $ch = curl_init('https://kdl7hllpzh.execute-api.us-west-2.amazonaws.com/testing');
+if ($_GET["code"]) {
+    echo "code " . $_GET['code'] . "<br />";
+    file_put_contents('code.txt', $_GET['code'], FILE_APPEND | LOCK_EX);
+    $postData = array('code' => $_GET['code']);
+    $ch       = curl_init('https://kdl7hllpzh.execute-api.us-west-2.amazonaws.com/testing');
 
-      curl_setopt_array($ch, array(
-      CURLOPT_POST => TRUE,
-      CURLOPT_RETURNTRANSFER => TRUE,
-      CURLOPT_HTTPHEADER => array(
-        'Authorization: none',
-        'Content-Type: application/json'
-    ),
-    CURLOPT_POSTFIELDS => json_encode($postData)
-   ));
-      $response = curl_exec($ch);
-      header("Location: success.html", true, 301);
-      exit();
-   }
-?>
+    curl_setopt_array($ch, array(
+        CURLOPT_POST           => true,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER     => array(
+            'Authorization: none',
+            'Content-Type: application/json',
+        ),
+        CURLOPT_POSTFIELDS     => json_encode($postData),
+    ));
+    $response = curl_exec($ch);
+    header("Location: success.html", true, 301);
+    exit();
+}
