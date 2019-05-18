c07fa7c342c16d0642f0dc821894b8bf64cfc3a2
SsRick <shankhashubhra96@gmail.com>
2019-05-18 23:49:15 +0530

    changed auth flow
    Not the form data will be used to signin and get auth token

--- lambda_code/authtokenexchange.py
+++ lambda_code/authtokenexchange.py
@@ -1,23 +1,16 @@
 from botocore.vendored import requests
 import json
-import boto3
-
-s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket = 'gauth-bucket-test'
-    key = 'cred.json'
-    tokenFile = 'firstToken.json'
-    response = s3.get_object(Bucket = bucket, Key = key)
-    jsonData = json.loads(response['Body'].read().decode('utf-8'))
-    API_ENDPOINT = jsonData["web"]["token_uri"]
+
+    API_ENDPOINT = event["token_uri"]
     print(event["code"])
     data = {'code':event["code"], 
-        'redirect_uri':jsonData["web"]["redirect_uris"][0], 
-        'client_id':jsonData["web"]["client_id"], 
-        'client_secret':jsonData["web"]["client_secret"],
+        'redirect_uri':event["redirect_uri"], 
+        'client_id':event["client_id"], 
+        'client_secret': event["client_secret"],
         'grant_type' : 'authorization_code',
-        'scope' : 'https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/userinfo.profile',
+        'scope' : event["scope"],
         'access_type' : 'offline'} 
 
     # sending post request and saving response as response object 
@@ -25,14 +18,9 @@ def lambda_handler(event, context):
 
     # extracting response text 
     response = r.text 
-    print(response)
+    # print(response)
+    return r.json()
     
-    body = json.dumps(response)
-    response = s3.put_object(Bucket = bucket,
-        Key = tokenFile,
-        Body = body,
-        ContentType='application/json')
-    # return response
 
 
 
--- lambda_code/gAuthTest.py
+++ lambda_code/gAuthTest.py
@@ -1,25 +1,19 @@
 import urllib.parse
 import json
-import boto3
 
-s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket = 'gauth-bucket-test'
-    key = 'cred.json'
-
-    response = s3.get_object(Bucket = bucket, Key = key)
-    jsonData = json.loads(response['Body'].read().decode('utf-8'))
-
-    client_id = jsonData['web']['client_id']
+    client_id = event['client_id']
     client_id = urllib.parse.quote(client_id, safe='')
 
-    auth_uri = jsonData['web']['auth_uri']
+    auth_uri = event['auth_uri']
 
-    redirect_uri = jsonData['web']['redirect_uris'][0]
+    redirect_uri = event['redirect_uri']
     redirect_uri = urllib.parse.quote(redirect_uri, safe='')
 
-    scope = 'https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/userinfo.profile'
+    # scope = 'https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/userinfo.profile'
+    # scope = 'https://www.googleapis.com/auth/drive.readonly'
+    scope = event['scope']
     scope = urllib.parse.quote(scope, safe='')
 
     part2 = 'client_id=' + client_id + '&redirect_uri=' + redirect_uri + '&scope=' + scope + '&access_type=offline&response_type=code'
